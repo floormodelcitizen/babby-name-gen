73a4557495873327deef90b6596d25720bf9af99
Chad Christopher <devluxv20@gmail.com>
2022-08-14 12:10:44 -0700

    Add Card dir and Name component #17

--- app.vue
+++ app.vue
@@ -62,12 +62,12 @@
       </button>
     </div>
     <div class="cards-container">
-      <div v-for="(name, index) in selectedNames" :key="name" class="card">
-        <h4>{{ name }}</h4>
-        <p>x</p>
-      </div>
+      <CardName
+        v-for="(name, index) in selectedNames"
+        :key="name"
+        :name="name"
+      />
     </div>
-    {{ selectedNames }}
   </div>
 </template>
 
@@ -109,24 +109,5 @@
     display: flex;
     margin-top: 3rem;
     flex-wrap: wrap;
-  }
-
-  .card {
-    background-color: rgb(27, 60, 138);
-    width: 28%;
-    color: white;
-    border-radius: 1rem;
-    padding: 0.1rem;
-    margin-right: 0.5rem;
-    margin-bottom: 1rem;
-    position: relative;
-  }
-
-  .card p {
-    position: absolute;
-    top: -25%;
-    left: 92.5%;
-    cursor: pointer;
-    color: rgba(255, 255, 255, 0.178);
   }
 </style>
