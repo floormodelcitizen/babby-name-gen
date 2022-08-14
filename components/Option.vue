f1b945a2f8bb611ae02bef2923701ade1e89ed65
Chad Christopher <devluxv20@gmail.com>
2022-08-14 11:16:47 -0700

    Computing class names Fixes #15

--- components/Option.vue
+++ components/Option.vue
@@ -15,6 +15,19 @@
   }
 
   const props = defineProps<OptionProps>();
+
+  const computeButtonClasses = (value, index) => {
+    const classNames = [];
+
+    if (props.options[props.option.category] === value) {
+      classNames.push("option-active");
+    }
+
+    if (index === 0) classNames.push("option-left");
+    if (index === props.option.buttons.length - 1)
+      classNames.push("option-right");
+    return classNames.join(" ");
+  };
 </script>
 
 <template>
@@ -22,10 +35,10 @@
     <h4>{{ option.title }}</h4>
     <div class="option-buttons">
       <button
-        v-for="value in option.buttons"
+        v-for="(value, index) in option.buttons"
         :key="value"
-        class="option option-left"
-        :class="options[option.category] === value && 'option-active'"
+        class="option"
+        :class="computeButtonClasses(value, index)"
         @click="options[option.category] = value"
       >
         {{ value }}
@@ -62,41 +75,5 @@
   .option-active {
     background-color: rgb(249, 87, 89);
     color: white;
-  }
-
-  .btn-primary {
-    background-color: rgb(249, 87, 89);
-    color: white;
-    border-radius: 6.5rem;
-    border: none;
-    padding: 0.75rem 4rem;
-    font-size: 1rem;
-    margin-top: 1rem;
-    cursor: pointer;
-  }
-
-  .cards-container {
-    display: flex;
-    margin-top: 3rem;
-    flex-wrap: wrap;
-  }
-
-  .card {
-    background-color: rgb(27, 60, 138);
-    width: 28%;
-    color: white;
-    border-radius: 1rem;
-    padding: 0.1rem;
-    margin-right: 0.5rem;
-    margin-bottom: 1rem;
-    position: relative;
-  }
-
-  .card p {
-    position: absolute;
-    top: -25%;
-    left: 92.5%;
-    cursor: pointer;
-    color: rgba(255, 255, 255, 0.178);
   }
 </style>
