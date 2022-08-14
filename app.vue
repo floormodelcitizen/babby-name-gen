6e07cf2f93fe40b38d8922398f34543880a71ec6
Chad Christopher <devluxv20@gmail.com>
2022-08-13 23:10:16 -0700

    Add Card component Fixes #11

--- app.vue
+++ app.vue
@@ -26,6 +26,24 @@
 
     selectedNames.value = filteredNames.map((name) => name.name);
   };
+
+  const optionsArray = [
+    {
+      title: "1) Choose a gender",
+      category: "gender",
+      buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
+    },
+    {
+      title: "2) Choose the name's popularity",
+      category: "popularity",
+      buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+    },
+    {
+      title: "3) Choose name's length",
+      category: "length",
+      buttons: [Length.SHORT, Length.ALL, Length.LONG],
+    },
+  ];
 </script>
 
 <template>
@@ -33,77 +51,7 @@
     <h1>Babby Name Generator</h1>
     <p>Choose your options and click the "Find Names" button below</p>
     <div class="options-container">
-      <div class="option-container">
-        <h4>1) Choose a gender</h4>
-        <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.gender === Gender.GIRL && 'option-active'"
-            @click="options.gender = Gender.GIRL"
-          >
-            Girl
-          </button>
-          <button
-            class="option"
-            :class="options.gender === Gender.UNISEX && 'option-active'"
-            @click="options.gender = Gender.UNISEX"
-          >
-            Unisex
-          </button>
-          <button
-            class="option option-right"
-            :class="options.gender === Gender.BOY && 'option-active'"
-            @click="options.gender = Gender.BOY"
-          >
-            Boy
-          </button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>2) Choose the name's popularity</h4>
-        <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.popularity === Popularity.TRENDY && 'option-active'"
-            @click="options.popularity = Popularity.TRENDY"
-          >
-            Trendy
-          </button>
-          <button
-            class="option option-right"
-            :class="options.popularity === Popularity.UNIQUE && 'option-active'"
-            @click="options.popularity = Popularity.UNIQUE"
-          >
-            Unique
-          </button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>3) Choose length</h4>
-        <div class="option-buttons">
-          <button
-            class="option option-left"
-            :class="options.length === Length.LONG && 'option-active'"
-            @click="options.length = Length.LONG"
-          >
-            Long
-          </button>
-          <button
-            class="option"
-            :class="options.length === Length.SHORT && 'option-active'"
-            @click="options.length = Length.SHORT"
-          >
-            Short
-          </button>
-          <button
-            class="option option-right"
-            :class="options.length === Length.ALL && 'option-active'"
-            @click="options.length = Length.ALL"
-          >
-            All
-          </button>
-        </div>
-      </div>
+      <Option v-for="option in optionsArray" :key="option.title" />
       <button class="btn-primary" @click="computeSelectedNames">
         Find Names
       </button>
